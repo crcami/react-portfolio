c104b9f454f9a31670af0f4f6b84d705e7eb8780
Camila Ramos <camilaafonsolemes@gmail.com>
2024-06-20 18:34:35 -0300

    Feat: Change in stylizations and content to be displayed in the project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,48 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import projectGlass from "../assets/projects/projectGlass.jpg";
+import projectGourmet from "../assets/projects/projectGourmet.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const HERO_CONTENT = `I'm a developer 
+ Junior front-end with a knack for creating robust and scalable web applications. With 1 year of practical experience, I have honed my skills in front-end technologies like html, css, JavaScript, React and Next.js, as well as back-end technologies like Node.js, My goal is to leverage my experience to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `With 1 year of professional experience, I worked with various technologies. My journey in web development began with a deep curiosity about how things work and has evolved into a career where I continually strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of programming, I like to stay active and explore new technologies.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    role: "Junior developer",
+    company: "Personal Projects",
+    description: `To improve my hard skills in Frontend, I have been coming since 
+January studying and putting into practice several projects. On them 
+I increasingly improve my skills in HTML, CSS, JavaScript, 
+frameworks like Bootstrap, JQuery and React. Developing
+skills also with Node and Express to be prepared 
+for any challenge.`,
+    technologies: ["Javascript", "React.js", "Next.js", "CSS", "HTML", "Bootstrap", "Jquery", "Node", "Express"],
   },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: project1,
+    title: "Project using glass effect",
+    image: projectGlass,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "This project demonstrates the use of HTML and CSS to create visually appealing cards with a glass effect.The cards contain a heading, subheading, descriptive text, and a link for further reading.",
+    technologies: ["HTML", "CSS", "JavaScript" ],
   },
   {
-    title: "Task Management App",
-    image: project2,
+    title: "Landing Page of a project aimed at a Goumet restaurant",
+    image: projectGourmet,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "This project consists of a landing page for a restaurant that stands out for offering a gourmet culinary experience with a focus on healthy eating.",
+    technologies: ["HTML", "CSS", "JavaScript", "React.js"],
   },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+  
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: " Rua Hildebrando de Araújo, Jardim Botanico ",
+  phoneNo: "41991738017 ",
+  email: "camilaafonsolemes@gmail.com",
 };
