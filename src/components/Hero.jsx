0ec330bf37154e3d6a0453df8e0e83ec5f970b42
Camila Ramos <camilaafonsolemes@gmail.com>
2024-06-27 11:13:43 -0300

    Feat: About Changes.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                     </h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 
                         to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                        Junior Web Developer
+                       Web Developer
                     </span>
                     <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
                         {HERO_CONTENT}
