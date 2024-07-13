a3e75205cd4b749dba211e359a1df5489b48f3ab
Camila Ramos <camilaafonsolemes@gmail.com>
2024-07-13 19:07:27 -0300

    feat: adding links to linkedin and github.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import logo from "../assets/images/logoBlue.png";
 import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
@@ -10,9 +9,12 @@ const Navbar = () => {
         <img className="mx-2 w-40 h-40" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaInstagram />
-        <FaGithub />
+        <a href="https://www.linkedin.com/in/crcami/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/crcami" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
       </div>
     </nav>
   );
