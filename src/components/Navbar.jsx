64a0e29e0e59451c34e144381deb69d8bb0c0207
Camila Ramos <camilaafonsolemes@gmail.com>
2024-07-13 16:54:21 -0300

    feat: final adjustments to framer motion details.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-  <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-40 h-40" src={logo} alt="logo" />
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaInstagram/>
-        <FaGithub/>
-    </div>
-  </nav>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaInstagram />
+        <FaGithub />
+      </div>
+    </nav>
   );
 };
 
